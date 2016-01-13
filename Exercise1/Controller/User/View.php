a91209fe4eb9d5f6492eb96a1bc0661f95836327
niranga <nr@netstarter.com>
2016-01-13 08:35:50 +0530

    2

--- Exercise1/Controller/User/View.php
+++ Exercise1/Controller/User/View.php
@@ -31,33 +31,30 @@ class View extends \Magento\Framework\App\Action\Action
         $page = $this->pageFactory->create();
 
         $data = $this->getRequest()->getParams();
+        $valid = "";
+        $boo = false;
 
-
-
-
-        if ($data['password'] != $data['password_confirmation']) {
-            $this->messageManager->addError(__('Please check your password.'));
-            $resultRedirect = $this->resultRedirectFactory->create();
-            $resultRedirect->setPath('exercise/user/create');
-            return $resultRedirect;
-
-        }else if($data['email']=='nirangamail@gmail.com'){
-            $this->messageManager->addError(__('Can not register yourselves'));
-            $resultRedirect = $this->resultRedirectFactory->create();
-            $resultRedirect->setPath('exercise/user/create');
-            return $resultRedirect;
+        if ($data['email'] == 'nirangamail@gmail.com') {
+            $valid .= 'Can not register yourselves';
+            $boo = true;
         }
-        else if($data['dob']< 18 ){
-            $this->messageManager->addError(__('uba podi ekek!!'));
+        if ($data['password'] != 'password_confirmation') {
+            $valid .= '| Please check your password.';
+            $boo = true;
+        }
+        if ($data['dob'] < 18) {
+            $valid .= '| Chk Age';
+            $boo = true;
+        }
+        if ($boo) {
+            $this->messageManager->addError($valid);
             $resultRedirect = $this->resultRedirectFactory->create();
             $resultRedirect->setPath('exercise/user/create');
             return $resultRedirect;
-        }
 
-        else {
+        } else {
             return $page;
         }
-
 
     }
 }
